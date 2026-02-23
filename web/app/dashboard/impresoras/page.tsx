f14474a44dee11d1a005f3bf6e7c96c8f9a3fa3c
JMontes <damon.1986@icloud.com>
2026-02-23 09:55:20 -0500

    Create page.tsx

---
+++ web/app/dashboard/impresoras/page.tsx
@@ -0,0 +1,10 @@
+import PrintersManager from '@/components/PrintersManager'
+
+export default function ImpresorasPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Impresoras</h1>
+      <PrintersManager />
+    </div>
+  )
+}
